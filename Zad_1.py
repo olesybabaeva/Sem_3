0092720dacfa8273a7662eb252aac6e218c8c378
Lesya <olesybabaeva@gmail.com>
2024-12-11 14:36:04 +0300

    задания
    выполнены

---
+++ Zad_1.py
@@ -0,0 +1,11 @@
+﻿#Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами. 
+# В результирующем списке не должно быть дубликатов.
+
+list1 = [3, 4, 4, 6, 14, 14, 25, 26, 26]
+for item in list1:
+    if list1.count(item) < 2:
+        for i in range(list1.count(item)):
+          list1.remove(item)
+list2 = set(list1)
+
+print(list2)
---
+++ Zad_3.py
@@ -0,0 +1,17 @@
+﻿#Создайте словарь со списком вещей для похода в качестве ключа и их массой в качестве значения. 
+# Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность. 
+# Достаточно вернуть один допустимый вариант.*Верните все возможные варианты комплектации рюкзака.
+
+things = {'спички': 1, 'компас': 2, 'термос': 5, 'аптечка': 4, 'палатка': 8, 'спальный мешок': 7, 'ложка': 2, 'тарелка': 2}
+
+def backpack_capacity(capacity: int, things: dict):
+    packaging_option = []
+    summary = []
+    for key, value in things.items():
+        if value <= capacity:
+            capacity -= value
+            packaging_option.append(key)
+    return packaging_option
+
+
+print(backpack_capacity(13, things))
